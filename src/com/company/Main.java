b53a81fea5c10e2c383104ec0212dc45215eedf8
Vladislav Plakhov <v.plakhov@haulmont.com>
2021-01-15 22:44:50 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[][] a = new double[3][5];
+        for (int i = 0; i < a.length; i++) {
+            System.out.println();
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]+" ");
+                a[i][j] = "asdasda";
+            }
+        }
+
+    }
+}
